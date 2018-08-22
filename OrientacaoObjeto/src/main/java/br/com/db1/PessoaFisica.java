d9dd34fd8f0b520ca9599e6d38b7709dc23d15eb
dwnziger <edanziger@outlook.com>
2018-08-22 17:40:34 -0300

    Atualizada com Pessoa Fisica e Juridica

---
+++ OrientacaoObjeto/src/main/java/br/com/db1/PessoaFisica.java
@@ -0,0 +1,18 @@
+package br.com.db1;
+
+public class PessoaFisica extends Pessoa {
+
+	private String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Boolean validadorCpf() {
+		return Boolean.TRUE;
+	}
+}
---
+++ OrientacaoObjeto/src/main/java/br/com/db1/PessoaJuridica.java
@@ -0,0 +1,18 @@
+package br.com.db1;
+
+public class PessoaJuridica extends Pessoa {
+
+	private String cnpj;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Boolean validadorCnpj() {
+		return Boolean.TRUE;
+	}
+}
